5be0938f872b0694170876e2b49055b27eaf194c
seen89586-afk <seen89586@gmail.com>
2026-06-25 02:00:13 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="IIoT Security Audit Tool",
+    page_icon="🏭",
+    layout="wide"
+)
+
+st.title("🏭 ICT FOR CYBERSECURITY")
+
+st.subheader("IIoT Security Audit Tool")
+
+st.write(
+"""
+Welcome to our university project.
+
+Use the menu on the left side to explore:
+
+⭐ IIoT Security Audit
+
+🔒 Password Checker
+
+⚠️ IIoT Attack Simulator
+
+🏭 IIoT Benefits
+
+👥 About Project
+"""
+)
