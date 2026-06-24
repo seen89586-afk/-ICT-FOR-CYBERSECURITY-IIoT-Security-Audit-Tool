c300ec0d1c3d5e9b16716a6b55f147071a8257a8
seen89586-afk <seen89586@gmail.com>
2026-06-25 02:28:12 +0500

    Update 5_About_Project.py

--- pages/5_About_Project.py
+++ pages/5_About_Project.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+st.title("👥 About Project")
+
+st.subheader("ICT FOR CYBERSECURITY: IIoT Security Audit Tool")
+
+st.write("Developed By")
+
+st.write("Muhammad Sameer - 25-ME-151 (leader)")
+st.write("Shabaz Malik - 25-ME-155")
+st.write("Zain Abbas - 25-ME-159")
