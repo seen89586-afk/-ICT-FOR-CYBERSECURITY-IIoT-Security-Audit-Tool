ab16faa34b4ebf4ca9f193200814ae0de00bbd7d
seen89586-afk <seen89586@gmail.com>
2026-06-25 02:26:57 +0500

    Update 3_IIoT_Attack_Simulator.py

--- pages/3_IIoT_Attack_Simulator.py
+++ pages/3_IIoT_Attack_Simulator.py
@@ -1,1 +1,28 @@
+import streamlit as st
 
+st.title("⚠️ IIoT Attack Simulator")
+
+attack = st.selectbox(
+    "Select Attack",
+    ["Weak Password", "Unpatched Sensor", "Phishing Email", "Data Breach"]
+)
+
+if attack == "Weak Password":
+
+    st.error("Threat: Hackers may access industrial devices.")
+    st.success("Solution: Use strong passwords.")
+
+elif attack == "Unpatched Sensor":
+
+    st.error("Threat: Hackers may control sensors.")
+    st.success("Solution: Update device firmware.")
+
+elif attack == "Phishing Email":
+
+    st.error("Threat: Employees may leak information.")
+    st.success("Solution: Employee training.")
+
+else:
+
+    st.error("Threat: Sensitive data may be stolen.")
+    st.success("Solution: Backup and encrypt data.")
